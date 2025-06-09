05232ff60f54ca525a8e831e9d7766f13ad22af2
Isac Yevisa <isacyevisa@gmail.com>
2025-06-09 09:35:30 +0300

    modified the file

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,7 +2,7 @@
 
 # Global Conversion Factors
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5  # Ensuring exact match for automated check
 
 # Conversion Functions
 def convert_to_celsius(fahrenheit):
