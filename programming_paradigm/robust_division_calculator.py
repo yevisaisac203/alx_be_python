2f535dbdfbd4e2c2b49ba88fb56dde0fe1284b34
Isac Yevisa <isacyevisa@gmail.com>
2025-06-13 15:53:34 +0300

    modified a file

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -6,7 +6,7 @@ def safe_divide(numerator, denominator):
 
         
         result = num / denom
-        return f"The result of the division is {result:.2f}"
+        return f"The result of the division is {result:}"
 
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."
